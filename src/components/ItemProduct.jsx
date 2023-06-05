9d7e4e6398dd38c7a7ed76d2702bf52673e2ed04
Fabi Tureo <fabianestebantureo@gmail.com>
2023-06-05 19:56:06 -0300

    Fix: 🐛 Implementing other example with another object and destructuring in Children Component

--- src/components/ItemProduct.jsx
+++ src/components/ItemProduct.jsx
@@ -6,17 +6,24 @@
 // Primero desestructuro el nombre de la prop que en este caso es "product" ✔️
 // Luego, asigno con dos puntos y entre llaves a aquellos valores que quiero desestructurar de la prop llamada "product"✔️
 
-export const ItemProduct = ({ product: { id, title } }) => {
+export const ItemProduct = ({ product: { id, title }, condition: { isAvailable, theme} }) => {
     // console.log({ product }); // ✔️
     // const { id, description } = product ✔️
+
+    // console.log({ condition }) // ✔️
+    // const { enable, isAvailable, theme, title } = condition // ✔️
+    // console.log({ enable, isAvailable, theme, title}) // ✔️
 
     //     // product.id ❗
     //     // product.title ❗
     //     // ...
     //     //props.product.id ❗
 
-    // console.log({ id })
-    // console.log({ title })
+    console.log({ id })
+    console.log({ title })
 
-    return <h2>{id}</h2>;
+    console.log({ isAvailable })
+    console.log({ theme })
+
+    return <h2>id</h2>;
 };
