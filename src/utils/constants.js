8b4c88e1fe011e912cf5d54a4fff99f91c06f9cd
Fabi Tureo <fabianestebantureo@gmail.com>
2023-06-12 20:04:31 -0300

    Feat: ✨ Creating a new file with constants to API requests

---
+++ src/utils/constants.js
@@ -0,0 +1,5 @@
+export const API_PRACTICE = {
+    FAKESTOREAPI_PRODUCTS: "https://fakestoreapi.com/products",
+    PLATZI_PRODUCTS: "https://api.escuelajs.co/api/v1/products",
+    JSONPLACEHOLDER_USERS: "https://jsonplaceholder.typicode.com/users",
+};
